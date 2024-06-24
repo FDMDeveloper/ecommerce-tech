a5f6bb02e5d2433b626a5ef2e7bb0bb0bd6eecd6
Francesco Matteo Depalma <157421031+FDMDeveloper@users.noreply.github.com>
2024-06-24 09:32:51 +0200

    Update prodModel.js

--- backend/models/prodModel.js
+++ backend/models/prodModel.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
+const Schema = mongoose.Schema; // Destructure for conciseness
 
-const prodSchema = new mongoose.Schema({
-    name: {type:String,required:true},
-    description: {type:String,required:true},
-    price: {type:Number,required:true},
-    image: {type:String,required:true},
-    category: {type:String,required:true}
-})
+const prodSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: Schema.Types.Buffer, required: true }, // Use Buffer for binary data
+  category: { type: String, required: true },
+});
 
-const prodModel = mongoose.models.prod || mongoose.model("prod",prodSchema)
-
+const prodModel = mongoose.models.prod || mongoose.model("prod", prodSchema);
 
 export default prodModel;
